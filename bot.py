e259bfadfd646a31ad5e70945776767dd0c7f001
Nahuan Barreto <nahuan89@gmail.com>
2023-12-01 15:22:22 -0300

    desacoplando as credencias

--- bot.py
+++ bot.py
@@ -1,23 +1,46 @@
+import logging
+from playwright.sync_api import Playwright, sync_playwright, expect
 import csv
-from playwright.sync_api import Playwright, sync_playwright
 
-def run(playwright: Playwright, cnpj_emissor, senha_emissor, data_emissao, cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico) -> None:
+# Configurar o logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto("https://www.nfse.gov.br/EmissorNacional/Login")
-    page.get_by_placeholder("CPF/CNPJ").click()
-    page.get_by_placeholder("CPF/CNPJ").fill(cnpj_emissor)
-    page.get_by_placeholder("Senha").click()
-    page.get_by_placeholder("Senha").fill(senha_emissor)
-    page.get_by_role("button", name="Entrar").click()
-    page.locator("#wgtAcessoRapido").get_by_role("link").first.click()
+    # Ler credenciais do arquivo credenciais.csv
+    with open('credenciais.csv', newline='', encoding='utf-8') as credenciais_file:
+        credenciais_reader = csv.DictReader(credenciais_file)
+        credenciais = next(credenciais_reader)
+        cnpj_emissor = credenciais['cnpj_emissor']
+        senha_emissor = credenciais['senha_emissor']
 
-    # Incio do loop
-    for row in zip(cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico):
-        cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico = row
+    # Ler dados de emissão do arquivo emissao_input.csv
+    with open('emissao_input.csv', newline='', encoding='utf-8') as emissao_input_file:
+        emissao_input_reader = csv.DictReader(emissao_input_file)
+        emissao_input = next(emissao_input_reader)
+        data_emissao = emissao_input['data_emissao']
+        cnpj_cliente = emissao_input['cnpj_cliente']
+        telefone_cliente = emissao_input['telefone_cliente']
+        email_cliente = emissao_input['email_cliente']
+        valor = emissao_input['valor']
+        servico_search = emissao_input['servico_search']
+        cnae_servico = emissao_input['cnae_servico']
+        descrica_servico = emissao_input['descrica_servico']
 
+    try:
+            
+        page.goto("https://www.nfse.gov.br/EmissorNacional/Login")
+        page.get_by_placeholder("CPF/CNPJ").click()
+        page.get_by_placeholder("CPF/CNPJ").fill(cnpj_emissor)
+        page.get_by_placeholder("Senha").click()
+        page.get_by_placeholder("Senha").fill(senha_emissor)
+        page.get_by_role("button", name="Entrar").click()
+        page.locator("#wgtAcessoRapido").get_by_role("link").first.click()
         page.locator("#DataCompetencia").click()
         page.locator("#DataCompetencia").fill(data_emissao)
         page.locator("#pnlTomador label").filter(has_text="Brasil").locator("i").click()
@@ -50,19 +73,17 @@ def run(playwright: Playwright, cnpj_emissor, senha_emissor, data_emissao, cnpj_
             page.get_by_role("link", name="Baixar DANFSe").click()
         download1 = download1_info.value
         page.get_by_role("link", name=" Nova NFS-e").click()
+        logger.info("Processo concluído com sucesso.")
+    except Exception as e:
+        logger.error(f"Erro durante a execução: {str(e)}")
 
-    context.close()
-    browser.close()
+    finally:
+        context.close()
+        browser.close()
 
+if __name__ == "__main__":
+    logging.info("Iniciando o Robô para Emitir faturasFaturas.")
+    
+    with sync_playwright() as playwright:
+        run(playwright)
 
-# Lê as informações do arquivo CSV
-with open('seuarquivo.csv', 'r', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    data = list(reader)
-
-with sync_playwright() as playwright:
-    for row in data:
-        # Convertendo a coluna 'valor' para um número
-        row['valor'] = float(row['valor'].replace(',', '.'))
-
-        run(playwright, **row)
