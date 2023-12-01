9aa287e9e115ca8b3051a2e745199acda226feee
Nahuan Barreto <nahuan89@gmail.com>
2023-12-01 13:42:30 -0300

    adicionando um loop e lendo arquivo csv

--- bot.py
+++ bot.py
@@ -1,28 +1,11 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+import csv
+from playwright.sync_api import Playwright, sync_playwright
 
-
-def run(playwright: Playwright) -> None:
+def run(playwright: Playwright, cnpj_emissor, senha_emissor, data_emissao, cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
 
-    # informações do prestador
-    cnpj_emissor = ""
-    senha_emissor = ''
-    data_emissao = '28/11/2023'
-
-
-    # informações do prestador
-
-    cnpj_cliente= ''
-    telefone_cliente = ""
-    email_cliente =""
-    valor = "100"
-    servico_search = '01.'
-    cnae_servico ="01.01.01 - Análise e"
-    descrica_servico = "Serviço de Desenvolvimento de Aplicativo para controle de áudio."
-   
-   
     page.goto("https://www.nfse.gov.br/EmissorNacional/Login")
     page.get_by_placeholder("CPF/CNPJ").click()
     page.get_by_placeholder("CPF/CNPJ").fill(cnpj_emissor)
@@ -30,43 +13,54 @@ def run(playwright: Playwright) -> None:
     page.get_by_placeholder("Senha").fill(senha_emissor)
     page.get_by_role("button", name="Entrar").click()
     page.locator("#wgtAcessoRapido").get_by_role("link").first.click()
-    page.locator("#DataCompetencia").click()
-    page.locator("#DataCompetencia").fill(data_emissao)
-    page.locator("#pnlTomador label").filter(has_text="Brasil").locator("i").click()
-    page.locator("#Tomador_Inscricao").click()
-    page.locator("#Tomador_Inscricao").fill(cnpj_cliente)
-    page.get_by_role("button", name="").click()
-    page.locator("#Tomador_Telefone").click()
-    page.locator("#Tomador_Telefone").fill(telefone_cliente)
-    page.locator("#Tomador_Telefone").press("Tab")
-    page.locator("#Tomador_Email").fill(email_cliente)
-    page.get_by_role("button", name="Avançar").click()
-    page.locator("#pnlLocalPrestacao").get_by_label("").click()
-    page.get_by_label("Search").fill("São Pa")
-    page.get_by_role("option", name="São Paulo/SP").click()
-    page.get_by_label("", exact=True).click()
-    page.get_by_label("Search").fill(servico_search)
-    page.get_by_role("option", name=cnae_servico).click()
-    page.locator("#pnlServicoPrestado").get_by_text("Não", exact=True).click()
-    page.locator("#ServicoPrestado_Descricao").click()
-    page.locator("#ServicoPrestado_Descricao").fill(descrica_servico)
-    page.get_by_role("button", name="Avançar").click()
-    page.locator("#Valores_ValorServico").click()
-    page.locator("#Valores_ValorServico").fill(valor)
-    page.get_by_role("button", name="Avançar").click()
-    page.locator("#btnProsseguir").click()
-    with page.expect_download() as download_info:
-        page.get_by_role("link", name="Baixar XML").click()
-    download = download_info.value
-    with page.expect_download() as download1_info:
-        page.get_by_role("link", name="Baixar DANFSe").click()
-    download1 = download1_info.value
-    page.get_by_role("link", name=" Nova NFS-e").click()
 
-    # ---------------------
+    # Incio do loop
+    for row in zip(cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico):
+        cnpj_cliente, telefone_cliente, email_cliente, valor, servico_search, cnae_servico, descrica_servico = row
+
+        page.locator("#DataCompetencia").click()
+        page.locator("#DataCompetencia").fill(data_emissao)
+        page.locator("#pnlTomador label").filter(has_text="Brasil").locator("i").click()
+        page.locator("#Tomador_Inscricao").click()
+        page.locator("#Tomador_Inscricao").fill(cnpj_cliente)
+        page.get_by_role("button", name="").click()
+        page.locator("#Tomador_Telefone").click()
+        page.locator("#Tomador_Telefone").fill(telefone_cliente)
+        page.locator("#Tomador_Telefone").press("Tab")
+        page.locator("#Tomador_Email").fill(email_cliente)
+        page.get_by_role("button", name="Avançar").click()
+        page.locator("#pnlLocalPrestacao").get_by_label("").click()
+        page.get_by_label("Search").fill("São Pa")
+        page.get_by_role("option", name="São Paulo/SP").click()
+        page.get_by_label("", exact=True).click()
+        page.get_by_label("Search").fill(servico_search)
+        page.get_by_role("option", name=cnae_servico).click()
+        page.locator("#pnlServicoPrestado").get_by_text("Não", exact=True).click()
+        page.locator("#ServicoPrestado_Descricao").click()
+        page.locator("#ServicoPrestado_Descricao").fill(descrica_servico)
+        page.get_by_role("button", name="Avançar").click()
+        page.locator("#Valores_ValorServico").click()
+        page.locator("#Valores_ValorServico").fill(valor)
+        page.get_by_role("button", name="Avançar").click()
+        page.locator("#btnProsseguir").click()
+        with page.expect_download() as download_info:
+            page.get_by_role("link", name="Baixar XML").click()
+        download = download_info.value
+        with page.expect_download() as download1_info:
+            page.get_by_role("link", name="Baixar DANFSe").click()
+        download1 = download1_info.value
+        page.get_by_role("link", name=" Nova NFS-e").click()
+
     context.close()
     browser.close()
 
 
+# Lê as informações do arquivo CSV
+with open('seuarquivo.csv', 'r', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    header = next(reader)  # Ignora o cabeçalho
+    data = list(reader)
+
 with sync_playwright() as playwright:
-    run(playwright)
+    for row in data:
+        run(playwright, *row)
