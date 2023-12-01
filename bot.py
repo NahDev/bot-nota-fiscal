854f13548fcbdce5274bcccba9da982be6b291c4
Nahuan Barreto <nahuan89@gmail.com>
2023-12-01 13:29:32 -0300

    correção do codigo

--- bot.py
+++ bot.py
@@ -5,45 +5,47 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
+    cnpj_emissor = "35.686.321/0001-76"
+    senha_emissor = ''
+    data_emissao = '28/11/2023'
+
+    cnpj_cliente= '395.820.738/37'
+    telefone_cliente = "(11)9874-56334"
+    email_cliente ="nahuan89@gmail.com"
+    valor = "100"
+    servico_search = '01.'
+    cnae_servico ="01.01.01 - Análise e"
+    descrica_servico = "Serviço de Desenvolvimento de Aplicativo para controle de áudio."
     page.goto("https://www.nfse.gov.br/EmissorNacional/Login")
     page.get_by_placeholder("CPF/CNPJ").click()
-    page.get_by_placeholder("CPF/CNPJ").fill("35.686.321/0001-76")
+    page.get_by_placeholder("CPF/CNPJ").fill(cnpj_emissor)
     page.get_by_placeholder("Senha").click()
-    page.get_by_placeholder("Senha").fill("Nhn.sk89")
+    page.get_by_placeholder("Senha").fill(senha_emissor)
     page.get_by_role("button", name="Entrar").click()
     page.locator("#wgtAcessoRapido").get_by_role("link").first.click()
-    page.locator("#btn_DataCompetencia").click()
-    page.get_by_role("cell", name="1", exact=True).first.click()
+    page.locator("#DataCompetencia").click()
+    page.locator("#DataCompetencia").fill(data_emissao)
     page.locator("#pnlTomador label").filter(has_text="Brasil").locator("i").click()
     page.locator("#Tomador_Inscricao").click()
-
-    #empresa prestadora de serviço
-    cnpj_contratante = '39582073837'
-    estado = 'São Paulo'
-    estado1 = 'São Paulo/SP'
-    servico = "01.01.01 - Análise e"
-    descricao_servico = "Serviço de Desenvolvimento de Aplicativo para controle de áudio."
-    valor_servico = "10000" # 100,00 reais, nesse caso
-    
-    page.locator("#Tomador_Inscricao").fill(cnpj_contratante)
-    page.locator("#Tomador_Inscricao").press("Tab")
+    page.locator("#Tomador_Inscricao").fill(cnpj_cliente)
+    page.get_by_role("button", name="").click()
+    page.locator("#Tomador_Telefone").click()
+    page.locator("#Tomador_Telefone").fill(telefone_cliente)
+    page.locator("#Tomador_Telefone").press("Tab")
+    page.locator("#Tomador_Email").fill(email_cliente)
     page.get_by_role("button", name="Avançar").click()
     page.locator("#pnlLocalPrestacao").get_by_label("").click()
-    page.get_by_label("Search").fill(estado)
-    page.get_by_role("option", name=estado1).click()
+    page.get_by_label("Search").fill("São Pa")
+    page.get_by_role("option", name="São Paulo/SP").click()
     page.get_by_label("", exact=True).click()
-    page.get_by_label("Search").fill(servico)
-    page.get_by_role("option", name=servico).click()
-    page.locator("#pnlServicoPrestado label").filter(has_text=re.compile(r"^Não$")).locator("i").click()
+    page.get_by_label("Search").fill(servico_search)
+    page.get_by_role("option", name=cnae_servico).click()
+    page.locator("#pnlServicoPrestado").get_by_text("Não", exact=True).click()
     page.locator("#ServicoPrestado_Descricao").click()
-    page.locator("#ServicoPrestado_Descricao").fill(descricao_servico)
-    page.locator("#ServicoPrestado_CodigoNBS_chosen a").click()
-    page.locator("#ServicoPrestado_CodigoNBS_chosen").get_by_role("textbox").fill("desen")
-    page.locator("#ServicoPrestado_CodigoNBS_chosen").get_by_text("112012200 - Serviços de").click()
+    page.locator("#ServicoPrestado_Descricao").fill(descrica_servico)
     page.get_by_role("button", name="Avançar").click()
     page.locator("#Valores_ValorServico").click()
-    page.locator("#Valores_ValorServico").fill(valor_servico)
-    page.locator("a").filter(has_text="Nenhum").click()
+    page.locator("#Valores_ValorServico").fill(valor)
     page.get_by_role("button", name="Avançar").click()
     page.locator("#btnProsseguir").click()
     with page.expect_download() as download_info:
@@ -52,8 +54,6 @@ def run(playwright: Playwright) -> None:
     with page.expect_download() as download1_info:
         page.get_by_role("link", name="Baixar DANFSe").click()
     download1 = download1_info.value
-    page.get_by_role("link", name=" Visualizar NFS-e").click()
-    page.goto("https://www.nfse.gov.br/EmissorNacional/DPS/NFSe?idr=Q3d5RkUrcG5veXRoV0NkOTVLYTQrUT090")
     page.get_by_role("link", name=" Nova NFS-e").click()
 
     # ---------------------
